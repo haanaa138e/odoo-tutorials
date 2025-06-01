1fbea1f7bc8338d58b76139c28712509b0207ccf
haanaa138e <hanakalateh@gmail.com>
2025-06-01 13:26:43 +0330

    [ADD] awesome_owl: add a dynamic attributes , todolist and card development,

--- awesome_owl/static/src/todo_list/todo_item.js
+++ awesome_owl/static/src/todo_list/todo_item.js
@@ -8,6 +8,16 @@ export class TodoItem extends Component {
         todo: {
             type: Object,
             shape: { id: Number, description: String, isCompleted: Boolean }
-        }
+        },
+        toggleState: Function,
+	    removeTodo: Function,
     };
+
+    onChange() {
+        this.props.toggleState(this.props.todo.id);
+    }
+	onRemove() {
+        this.props.removeTodo(this.props.todo.id);
+    }
+
 }
