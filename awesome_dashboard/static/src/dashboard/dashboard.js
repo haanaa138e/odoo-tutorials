455c251bb0ccc551dc04f1514912941b14f387f8
haanaa138e <hanakalateh@gmail.com>
2025-06-09 17:05:43 +0330

    [ADD] awesome_dashboard: Exercises 8.Lazy loading the dashboard

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -39,4 +39,4 @@ class AwesomeDashboard extends Component {
 }
 
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
