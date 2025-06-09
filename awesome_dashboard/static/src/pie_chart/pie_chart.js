2f509e6d16e806410bc0cba358fbaa6965eac920
haanaa138e <hanakalateh@gmail.com>
2025-06-09 12:30:59 +0330

    [FIX] awesome_dashboard: Exercises 6. Display a pie chart

---
+++ awesome_dashboard/static/src/pie_chart/pie_chart.js
@@ -0,0 +1,49 @@
+/** @odoo-module */
+
+import { Component, onWillStart, useRef, onMounted, onWillUnmount } from "@odoo/owl";
+import { loadJS } from "@web/core/assets";
+
+
+export class PieChart extends Component {
+    static template = "awesome_dashboard.PieChart";
+
+    static props = {
+        label: String,
+        data: Object,
+    };
+
+    setup() {
+        this.canvasRef = useRef("canvas");
+        onWillStart(() => loadJS(["/web/static/lib/Chart/Chart.js"]));
+        onMounted(() => {
+            this.renderChart();
+        });
+        onWillUnmount(() => {
+            this.chart.destroy();
+        });
+    }
+
+    renderChart() {
+        const labels = Object.keys(this.props.data);
+        const data = Object.values(this.props.data);
+	    const colorMap = {
+		    s: "#FC9611",
+		    m: "#257BFD",
+		    xl: "#95BCF5",
+	    };
+		const color = labels.map(label => colorMap[label] );
+        this.chart = new Chart(this.canvasRef.el, {
+            type: "pie",
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        label: this.props.label,
+                        data: data,
+                        backgroundColor: color,
+                    },
+                ],
+            },
+        });
+    }
+}
