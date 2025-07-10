9910c810435af7c855d81f2dcb51d3d08ec8f572
haanaa138e <hanakalateh@gmail.com>
2025-07-10 14:42:52 +0330

    [ADD] awesome_clicker: Exercise 10. Notify when a milestone is reached

--- awesome_clicker/static/src/clicker_model.js
+++ awesome_clicker/static/src/clicker_model.js
@@ -1,4 +1,5 @@
 import { Reactive } from "@web/core/utils/reactive";
+import { EventBus } from "@odoo/owl";
 
 export class ClickerModel extends Reactive{
 	  constructor() {
@@ -6,7 +7,7 @@ export class ClickerModel extends Reactive{
 		  this.clicker = 0;
 		  this.level = 0;
 		  this.clickBots = 0;
-
+		  this.bus = new EventBus();
 
 		  document.addEventListener("click",() => this.increment(1),true);
 		  setInterval(() => {
@@ -19,6 +20,7 @@ export class ClickerModel extends Reactive{
 		  this.clicker += inc;
 
 		  if (this.level < 1 && this.clicker >= 1000) {
+			  this.bus.trigger("MILESTONE_1k");
 			  this.level++;
 		  }
 	  }
