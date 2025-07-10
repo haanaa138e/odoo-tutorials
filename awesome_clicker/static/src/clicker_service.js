0a3c0c6e05a4393cb1d84f06fe7ca775cfbbb077
haanaa138e <hanakalateh@gmail.com>
2025-07-10 12:16:33 +0330

    [ADD] awesome_clicker: Exercise 8. Buy ClickBots.

--- awesome_clicker/static/src/clicker_service.js
+++ awesome_clicker/static/src/clicker_service.js
@@ -4,11 +4,32 @@ import { reactive } from "@odoo/owl";
 const clickerService = {
 
     start(env) {
-		const  state = reactive({ clicker: 0 });
+		 const state = reactive({
+            clicker: 0,
+            level: 0,
+            clickBots: 0,
+        });
+
+        setInterval(() => {
+            state.clicker += state.clickBots * 10;
+        }, 10000);
 
 
 		function increment(inc){
 			state.clicker += inc;
+
+			  if (state.level < 1 && state.clicker >= 1000) {
+                state.level++;
+            }
+        }
+
+        function buyClickBot() {
+            const clickBotPrice = 1000;
+            if (state.clicker < clickBotPrice) {
+                return false;
+            }
+            state.clicker -= clickBotPrice;
+            state.clickBots += 1;
 		}
 
 		document.addEventListener("click",() => increment(1),true)
@@ -16,6 +37,7 @@ const clickerService = {
 	    return{
 			state,
 		    increment,
+		    buyClickBot,
 	    };
 
     },
