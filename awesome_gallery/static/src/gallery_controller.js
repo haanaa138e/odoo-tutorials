bf580fe481a13ad8cb45020d1756af0fd2d14f90
haanaa138e <hanakalateh@gmail.com>
2025-07-14 09:33:46 +0330

    [ADD] awesome_gallery: Exercise 5. Solve the concurrency problem.

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -2,6 +2,7 @@ import {Layout} from "@web/search/layout";
 import { standardViewProps } from "@web/views/standard_view_props";
 import { useService } from "@web/core/utils/hooks";
 import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
+import { KeepLast } from "@web/core/utils/concurrency";
 
 export class GalleryController extends Component {
 	 static template = "awesome_gallery.GalleryController";
@@ -14,6 +15,7 @@ export class GalleryController extends Component {
 	 setup() {
         this.orm = useService("orm");
         this.images = useState({ data: [] });
+		this.keeplast = new KeepLast();
         onWillStart(async () => {
             const { records } = await this.loadImages(this.props.domain);
             this.images.data = records;
@@ -28,15 +30,17 @@ export class GalleryController extends Component {
     }
 
     loadImages(domain) {
-        return this.orm.webSearchRead(this.props.resModel, domain, {
-            limit: this.props.archInfo.limit,
-            specification: {
-                [this.props.archInfo.imageField]: {},
-            },
-            context: {
-                bin_size: true,
-            }
-        });
+        return this.keeplast.add(
+            this.orm.webSearchRead(this.props.resModel, domain, {
+                limit: this.props.archInfo.limit,
+                specification: {
+                    [this.props.archInfo.imageField]: {},
+                },
+                context: {
+                    bin_size: true,
+                }
+            })
+        );
     }
 }
 
