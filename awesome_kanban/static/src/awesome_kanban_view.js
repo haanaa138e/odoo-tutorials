9b24823f11b6303b89d673cd38d00f2759ccc273
Hanane Kalateh <hanakalateh@gmail.com>
2025-07-15 09:40:27 +0330

    [ADD] awesome_kanban: Exercise 1. Create a new kanban view.

--- awesome_kanban/static/src/awesome_kanban_view.js
+++ awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,3 +1,10 @@
-/** @odoo-module */
+import { kanbanView } from "@web/views/kanban/kanban_view";
+import { registry } from "@web/core/registry";
+import { AwesomeKanbanController } from "./kanban_controller.JS";
 
-// TODO: Define here your AwesomeKanban view
+const awesomeKanbanController = {
+    ...kanbanView,
+    Controller: AwesomeKanbanController,
+};
+
+registry.category("views").add("awesome_kanban", awesomeKanbanController);
