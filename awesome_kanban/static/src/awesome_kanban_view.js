2c31dbcf55ce50a659e3d37106d0d545351e87b2
Hanane Kalateh <hanakalateh@gmail.com>
2025-07-15 10:21:57 +0330

    [ADD] awesome_kanban: Exercise 2. Create a CustomerList component.

--- awesome_kanban/static/src/awesome_kanban_view.js
+++ awesome_kanban/static/src/awesome_kanban_view.js
@@ -1,6 +1,6 @@
 import { kanbanView } from "@web/views/kanban/kanban_view";
 import { registry } from "@web/core/registry";
-import { AwesomeKanbanController } from "./kanban_controller.JS";
+import { AwesomeKanbanController } from "./kanban_controller.js";
 
 const awesomeKanbanController = {
     ...kanbanView,
---
+++ awesome_kanban/static/src/kanban_controller.js
@@ -0,0 +1,15 @@
+import {KanbanController } from "@web/views/kanban/kanban_controller";
+import { CustomerList } from "./customer_list";
+
+export class AwesomeKanbanController extends KanbanController {
+	static components = { ...KanbanController.components, CustomerList}
+    static template = "awesome_kanban.AwesomeKanbanController";
+
+    setup() {
+        super.setup();
+    }
+
+    selectCustomer(customer_id) {
+        console.log(customer_id);
+    }
+}
