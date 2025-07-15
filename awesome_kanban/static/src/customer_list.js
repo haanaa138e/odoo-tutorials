429bf6971d9d298bc2da5bb0995b4f7c4d22325c
Hanane Kalateh <hanakalateh@gmail.com>
2025-07-15 13:12:46 +0330

    [ADD] awesome_kanban: Exercise 6. Add a search bar to the customer list.

--- awesome_kanban/static/src/customer_list.js
+++ awesome_kanban/static/src/customer_list.js
@@ -1,6 +1,7 @@
 import { Component, onWillStart, useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { KeepLast } from "@web/core/utils/concurrency";
+import { fuzzyLookup } from "@web/core/utils/search";
 
 export class CustomerList extends Component {
     static template = "awesome_kanban.CustomerList";
@@ -13,11 +14,14 @@ export class CustomerList extends Component {
 	 setup() {
         this.orm = useService("orm");
 		this.partners = useState({ data: [] });
+		this.displayedPartners = useState({ data: [] });
+        this.filterName = "";
 		this.keepLast = new KeepLast();
 
 
         onWillStart(async () => {
 			this.partners.data = await this.loadCustomers([]);
+			this.displayedPartners.data = this.partners.data;
         })
     }
 
@@ -25,8 +29,25 @@ export class CustomerList extends Component {
         const checked = ev.target.checked
         const domain = checked ?  [["opportunity_ids", "!=", false]] : [];
         this.partners.data = await this.keepLast.add(this.loadCustomers(domain));
+        this.filterCustomers(this.filterName);
     }
 
+    onCustomerFilter(ev) {
+        this.filterName = ev.target.value;
+        this.filterCustomers(ev.target.value);
+    }
+
+    filterCustomers(name) {
+        if (name) {
+            this.displayedPartners.data = fuzzyLookup(
+                name,
+                this.partners.data,
+                (partner) => partner.display_name
+            );
+        } else {
+            this.displayedPartners.data = this.partners.data;
+        }
+    }
 
     loadCustomers(domain) {
         return this.orm.searchRead("res.partner", domain, ["display_name"]);
